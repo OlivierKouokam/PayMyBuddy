aa4aa01a988f13114e5b4ee4df72c3f4461525ce
OlivierKouokam <62020582+OlivierKouokam@users.noreply.github.com>
2026-04-02 15:53:24 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
-#!/bin/bash
-
-java -jar /app/paymybuddy.jar
+#!/bin/sh
+exec java -jar /app/paymybuddy.jar
 
 # Démarrer l'application et rediriger les logs
 #java -jar /app/paymybuddy.jar >> /app/logs/application.log 2>&1
